1b28753b361ed264be96df5ed0913f089cec7638
EvanPrograms <eperry2688@gmail.com>
2026-02-22 00:58:22 -0700

    Favicon, sitemap, robots

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
+
+const baseUrl = "https://somanyrosesmaidservices.com";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
